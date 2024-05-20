d59f2b9f648b38264f1041b4a5a049b3bc572df3
TSAI, CHENG-JUNG <37261399+arrontsai@users.noreply.github.com>
2024-05-20 16:36:54 +0800

    Time: 51 ms (47.98%) | Memory: 17.1 MB (33.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,18 +3,17 @@ class Solution:
         # 用hashmap去算每個字母出現幾次，如過s存完後，t可以把所有的hashmap都削掉就是正確的
         # 特殊狀況，不用檢查直接回傳
         if len(s) != len(t): return False
-        hashMap = {}
+        hashMapS = {}
+        hashMapT = {}
         for i in range(len(s)):
-            if s[i] not in hashMap.keys():
-                hashMap[s[i]] = 1
+            if s[i] not in hashMapS.keys():
+                hashMapS[s[i]] = 1
             else:
-                hashMap[s[i]] += 1
-        for j in range(len(t)):
-            if t[j] not in hashMap.keys():
-                return False
+                hashMapS[s[i]] += 1
+        for i in range(len(t)):
+            if t[i] not in hashMapT.keys():
+                hashMapT[t[i]] = 1
             else:
-                hashMap[t[j]] -= 1
-        for num in hashMap.values():
-            if num != 0:
-                return False
-        return True
+                hashMapT[t[i]] += 1
+        return hashMapS == hashMapT
+        

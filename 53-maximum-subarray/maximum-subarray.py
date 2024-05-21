39eb3a158dd2b8bf951d2458c040ff7c293f472f
TSAI, CHENG-JUNG <37261399+arrontsai@users.noreply.github.com>
2024-05-21 16:20:06 +0800

    Time: 587 ms (6.72%) | Memory: 31 MB (64.05%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,12 +1,13 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        #要有一個參數存目前最大的值
-        #另一個參數去往前加，當變成負的，就再從當下的index開始
-        maxSum = curSum = nums[0]
+        #用dp解決這個問題，試試看！
+        #到i的所有subarray最大的是多少，存在dp裡面
+        # initialize..
+        dp = []
+        dp.append(nums[0])
+        maxSum = dp[0]
         for i in range(1,len(nums)):
-            if curSum >= 0:
-                curSum += nums[i]
-            else:
-                curSum = nums[i]
-            maxSum = max(maxSum, curSum)
+            dp.append(max(dp[i-1]+nums[i],nums[i]))
+            maxSum = max(dp[i],maxSum)
         return maxSum
+        

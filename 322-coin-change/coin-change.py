98795aaa9843a11a6496e7f4e3d62bfb12daac89
TSAI, CHENG-JUNG <37261399+arrontsai@users.noreply.github.com>
2024-06-04 16:48:48 +0800

    Time: 1144 ms (17.72%) | Memory: 31.6 MB (19.16%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -2,13 +2,18 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         # x + 2y + 5z = amount
         # dp 紀錄 amount所需的coin_num
-        minCoins = [math.inf for _ in range(amount + 1)]
-        minCoins[0] = 0
-        # example dp[11] = min(dp[10]+[1, 0, 0], dp[9] + [0, 1, 0], dp[6] + [0, 0, 1])
-        for i in range(1, amount + 1):
-            for coin in coins:
-                if coin <= i:
-                    minCoins[i] = min(minCoins[i],minCoins[i-coin]+1)
-        if minCoins[amount] == math.inf:
-            return -1
-        return minCoins[amount]
+        # example dp[11] = min(dp[10]+1, dp[9] + 1, dp[6] + 1)
+        def subFunction(amount, cache):
+            if amount < 0:
+                return math.inf
+            elif amount == 0:
+                return 0
+            elif amount in cache:
+                return cache[amount]
+            cache[amount] = min(subFunction(amount - c , cache)+1 for c in coins)
+            return cache[amount]
+        ans = subFunction(amount, {})
+        return ans if ans!= math.inf else -1
+
+
+        

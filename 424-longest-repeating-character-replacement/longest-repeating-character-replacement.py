d9a2bc73ae001efa93c833431cf239fcbc43de75
TSAI, CHENG-JUNG <37261399+arrontsai@users.noreply.github.com>
2024-06-15 17:42:07 +0800

    Time: 83 ms (72.18%) | Memory: 16.5 MB (79.08%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,25 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        
-        l = 0
-        c_frequency = {}
-        longest_str_len = 0
-        for r in range(len(s)):
-            
-            if not s[r] in c_frequency:
-                c_frequency[s[r]] = 0
-            c_frequency[s[r]] += 1
-            
-            # Replacements cost = cells count between left and right - highest frequency
-            cells_count = r - l + 1
-            if cells_count - max(c_frequency.values()) <= k:
-                longest_str_len = max(longest_str_len, cells_count)
-                
+        l, r = 0, 0
+        long_len = -float('inf')
+        map = collections.defaultdict(int)
+        while r < len(s):
+            map[s[r]] += 1
+            cell_count = r - l + 1
+            if cell_count - max(map.values())<=k:
+                long_len = max(long_len, cell_count)
             else:
-                c_frequency[s[l]] -= 1
-                if not c_frequency[s[l]]:
-                    c_frequency.pop(s[l])
+                map[s[l]] -= 1
                 l += 1
-        
-        return longest_str_len
+            r += 1
+        return long_len
+
 		

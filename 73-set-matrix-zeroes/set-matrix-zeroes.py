b411245aa4c9b6b554e6e5fb7bb4d41f2aaa1c9e
TSAI, CHENG-JUNG <37261399+arrontsai@users.noreply.github.com>
2024-07-29 13:27:41 +0800

    Time: 96 ms (88.42%) | Memory: 17.4 MB (84.36%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,17 +5,34 @@ class Solution:
         """
         row = len(matrix)
         col = len(matrix[0])
-        flag_row = [False] * row
-        flag_col = [False] * col
+        first_row_got_zero = False
+        first_col_got_zero = False
+
         for m in range(row):
             for n in range(col):
                 if matrix[m][n] == 0:
-                    flag_row[m] = True
-                    flag_col[n] = True
-        for i in range(len(flag_row)):
-            for j in range(len(flag_col)):
-                if flag_row[i] == True or flag_col[j] == True:
+                    if m == 0:
+                        first_row_got_zero = True
+                    if n == 0:
+                        first_col_got_zero = True
+                    matrix[0][n] = matrix[m][0] = 0
+
+        for i in range(1,row):
+            for j in range(1,col):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
                     matrix[i][j] = 0
+                else:
+                    matrix[i][j]
+
+        if first_col_got_zero:
+            for i in range(row):
+                matrix[i][0] = 0
+        
+        if first_row_got_zero:
+            for j in range(col):
+                matrix[0][j] = 0
+
+
 
 
         
